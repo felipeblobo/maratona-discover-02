1f9e1d0ccc7f4cf076f07f24abfe7c1a7cbb6df8
Felipe Lobo <felipeblobo@gmail.com>
2021-03-27 11:58:32 -0300

    desenvolvendo projeto

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const routes = express.Router()
 
+const profile = {
+  name: 'Felipe',
+  avatar: 'https://avatars.githubusercontent.com/u/69439442?s=400&u=60eef3d20a9252062651358f05a66ed6e73e2876&v=4',
+  "monthly-budget": 3500,
+  "days-per-week": 5,
+  "hours-per-day": 4,
+  "vacation-per-year": 4
+}
+
 routes.get('/', (req, res) => {
   res.render('index')
 })
@@ -14,7 +23,7 @@ routes.get('/job/edit', (req, res) => {
 })
 
 routes.get('/profile', (req, res) => {
-  res.render('profile')
+  res.render('profile', {profile})
 })
 
 module.exports = routes
