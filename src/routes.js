cf90025511e916cf632c1ee218e82a8766ea1ef7
Felipe Lobo <felipeblobo@gmail.com>
2021-03-27 11:12:44 -0300

    criando page-header

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const routes = express.Router();
+const express = require('express')
+const routes = express.Router()
 
-routes.get("/", (req, res) => {
-    res.render("index");
-  });
-  
+routes.get('/', (req, res) => {
+  res.render('index')
+})
+
 routes.get('/job', (req, res) => {
-    res.render("job");
-  });
-  
+  res.render('job')
+})
+
 routes.get('/job/edit', (req, res) => {
-    res.render("job-edit");
-  });
-  
+  res.render('job-edit')
+})
+
 routes.get('/profile', (req, res) => {
-    res.render("profile");
-  });
-  
-module.exports = routes;
+  res.render('profile')
+})
+
+module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const server = express();
-const port = 3000;
-const routes = require('./routes');
+const express = require('express')
+const server = express()
+const port = 3000
+const routes = require('./routes')
 
-server.use(express.static("public"));
+server.use(express.static('public'))
 
-server.set('view engine', 'ejs');
+server.set('view engine', 'ejs')
 
-server.use(routes);
+server.use(routes)
 
-server.listen(port, console.log(`Servidor rodando na porta ${port}.`));
+server.listen(port, console.log(`Servidor rodando na porta ${port}.`))
