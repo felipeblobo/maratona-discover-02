c3b6bf609056f15c87781f18590890d8c2668bab
Felipe Lobo <felipeblobo@gmail.com>
2021-03-31 16:13:33 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -34,6 +34,9 @@ routes.get('/', (req, res) => {
     const remainingDays = Math.round(job['total-hours'] / job['daily-hours'])
     const createdDate = new Date(job.created_at)
     const finalDay = createdDate.getDate() + Number(remainingDays)
+    const finalDate = createdDate.setDate(finalDay)
+
+    const timeDiferrence = finalDate - Date.now()
 
     return job
   })
